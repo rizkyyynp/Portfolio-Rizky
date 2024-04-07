6c96d2591397e7bfd7c2f0207258f6c812a6fa08
Rizky Nugraha Pradana <rizkynp990@gmail.com>
2024-04-08 03:31:59 +0700

    change navbar link, and update css for myskill.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       colors:{
         primary: '#14b8a6',
         dark: '#0f172a',
+        darkBlue: '#071952',
+        logoJS: '#cec80d',
+        logoHTML: '#fa7000',
+        logoCSS: '#13c1ec',
+        logoDB: '#165cd4',
+        logoReact: '#03d3fc',
+        logoWP: '#13b8cd',
+        logoHTML: '#fa7000',
       },
       container: {
         center: true,
