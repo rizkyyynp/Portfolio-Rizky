f69c79105dc68aa81b11cd1589ecd2905048f936
Rizky Nugraha Pradana <rizkynp990@gmail.com>
2024-04-08 02:02:41 +0700

    Change index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: theme => ({
-        'custom-background': "url('./src/img/photo3.png')",
-      })
+      colors:{
+        primary: '#14b8a6',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
     },
   },
   plugins: [],
